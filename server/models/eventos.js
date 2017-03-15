63de2d48a6a12e35be4c304fd4e1a259e6b838da
Diego Vinicius <diegowebd@hotmail.com>
2017-03-15 00:09:12 -0300

    Encapsulating Db access to Models

---
+++ server/models/eventos.js
@@ -0,0 +1,54 @@
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:123@ds131340.mlab.com:31340/eventos', ['eventos']);
+
+/**
+ * ADD new evento from database
+ */
+exports.insert = (evento, callback) => {
+    db.eventos.save(evento, (err, evento) => {
+        if (err) 
+            return callback(err);
+        return callback(null, evento);
+    });
+};
+
+/**
+ * Obtain all eventos from database
+ */
+exports.all = (callback) => {
+    db.eventos.find((err, eventos) => {
+        if (err) 
+            return callback(err);
+        return callback(null, eventos);
+    });
+};
+
+/**
+ * Get one evento from database
+ */
+exports.filter = (id, callback) => {    
+    db.eventos.findOne({ _id: mongojs.ObjectId(id) },(err, evento) => {
+        if (err) 
+            return callback(err);
+        return callback(null, evento);
+    });
+};
+
+/**
+ * Remove one object from database 
+ */
+exports.remove = (id, callback) => {    
+    db.eventos.remove({ _id: mongojs.ObjectId(id) },(err, evento) => {
+        if (err) 
+            return callback(err);
+        return callback(null, evento);
+    });
+};
+
+exports.update = (id, evento, callback) => {    
+    db.eventos.update({ _id: mongojs.ObjectId(id) }, evento, {},(err, evento) => {
+        if (err) 
+            return callback(err);
+        return callback(null, evento);
+    });
+};
--- server/routes/eventos.js
+++ server/routes/eventos.js
@@ -1,8 +1,7 @@
 var express = require('express');
-var mongojs = require('mongojs');
+var model = require('../models/eventos.js');
 
 const router = express.Router();
-var db = mongojs('mongodb://admin:123@ds131340.mlab.com:31340/eventos', ['users']);
 
 var isValid = (req) => {
     const control = ['title', 'location', 'start_date', 'description'];
@@ -19,12 +18,14 @@ var isValid = (req) => {
  * Eventos Router
  */
 router.route('/')
+
     .get((req, res) => {        
-        db.eventos.find((err, eventos) => {
+        model.all((err, eventos) => {
             if(err) throw err;
-            res.json(eventos);            
+            res.json(eventos);
         });
     })
+
     .post((req, res, next) => {
         var evento = req.body;
 
@@ -32,28 +33,31 @@ router.route('/')
             return next(res.status(400).json({"error": "Bad Data"}));
         }
 
-        if (!evento.end_date) evento.end_date = evento.start_date;
-        
-        db.eventos.save(evento, function(err, evento){
+        if (!evento.end_date) evento.end_date = evento.start_date;            
+
+        model.insert(evento, (err, evento) => {
             if(err) throw err;
             res.json(evento);
         });
-        
+
     });
 
 router.route('/:id')
-    .get((req, res) => {        
-        db.eventos.findOne({ _id: mongojs.ObjectId(req.params.id) },(err, evento) => {
+
+    .get((req, res) => {                
+        model.filter(req.params.id, (err, evento) => {
             if(err) throw err;
             res.json(evento);
         });
     })
+
     .delete((req, res) => {
-        db.eventos.remove({ _id: mongojs.ObjectId(req.params.id) },(err, evento) => {
+        model.remove(req.params.id, (err, evento) => {
             if(err) throw err;
             res.json(evento);
         });
     })
+
     .put((req, res, next) => {
         var evento = req.body;
         
@@ -61,10 +65,9 @@ router.route('/:id')
             return next(res.status(400).json({"error": "Bad Data"}));
         }
         
-        db.eventos.update({ _id: mongojs.ObjectId(req.params.id) }, 
-            evento, {}, (err, evento) => {
-                if(err) throw err;
-                res.json(evento);
+        model.update(req.params.id, evento, (err, evento) => {
+            if(err) throw err;
+            res.json(evento);
         });
                 
     });
