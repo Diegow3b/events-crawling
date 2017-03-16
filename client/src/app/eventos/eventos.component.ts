7138ad3750ad2cb701c966625ecf73dd8a67cbf0
Diego Vinicius <diegowebd@hotmail.com>
2017-03-16 15:57:07 -0300

    Fixing problem when Delete and Update object

--- client/src/app/eventos/eventos.component.ts
+++ client/src/app/eventos/eventos.component.ts
@@ -1,15 +1,102 @@
 import { Component, OnInit } from '@angular/core';
+import { EventosService } from '../services/eventos.service';
+import { Evento } from '../../../class/evento';
 
 @Component({
-  selector: 'app-eventos',
-  templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+    selector: 'app-eventos',
+    templateUrl: './eventos.component.html',
+    styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+    eventos: Evento[];
+    _id: string;
+    title: string;
+    location: string;
+    start_date: string;
+    end_date: string;
+    slug: string;
+    description: string;
 
-  ngOnInit() {
-  }
+    constructor(private eventosService: EventosService) {
+        this.eventosService.getEventos()
+            .subscribe(eventos => {
+                this.eventos = eventos;
+            });
+    }
+
+    limparForm(){
+      this.title = '';
+      this.location = '';
+      this.start_date = '';
+      this.end_date = '';
+      this.slug = '';
+      this.description = '';
+    }
+
+    addEvento(event) {
+        event.preventDefault();
+        var newEvento = {
+            title: this.title,
+            location: this.location,
+            start_date: this.start_date,
+            end_date: this.end_date,
+            slug: this.slug,
+            description: this.description
+        }
+
+        this.eventosService.addEvento(newEvento)
+            .subscribe(evento => {
+                this.eventos.push(evento);
+                this.limparForm();
+            });
+
+    }
+
+    deleteEvento(id) {
+        var eventos = this.eventos;
+
+        this.eventosService.deleteEvento(id).subscribe(data => {
+            for (var i = 0; i < eventos.length; i++) {
+                if (eventos[i]._id == id) {
+                    eventos.splice(i, 1);
+                }
+            }
+        });
+    }
+
+    updateEvento() {
+        let _evento = {
+            _id: this._id,
+            title: this.title,
+            location: this.location,
+            start_date: this.start_date,
+            end_date: this.end_date,
+            slug: this.slug,
+            description: this.description
+        };
+        this.eventosService.updateEvento(_evento)
+            .subscribe(data => {
+                // TO DO Mostrar Mensagem de Alteração com Sucesso
+                var eventos = this.eventos;
+
+                for (var i = 0; i < eventos.length; i++) {
+                    if (eventos[i]._id.toString() == _evento._id) eventos[i].title = _evento.title
+                }
+                this.limparForm();
+            })
+    }
+
+    DOMloadEditEvento(evento) {
+        this._id = evento._id;
+        this.title = evento.title;
+        this.location = evento.location;
+        this.start_date = evento.start_date;
+        this.end_date = evento.end_date;
+        this.description = evento.description;
+    }
+
+    ngOnInit() {
+    }
 
 }
---
+++ client/src/app/services/eventos.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class EventosService {
+
+  constructor(private http: Http) {
+    console.log('Evento Service Initialized...');
+  }
+
+  getEventos() {
+      return this.http.get('http://localhost:3000/api/eventos')
+          .map(res => res.json());
+  }
+
+  addEvento(newEvento) {
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      return this.http.post('http://localhost:3000/api/eventos', JSON.stringify(newEvento), { headers: headers })
+          .map(res => res.json());
+  }
+
+  deleteEvento(id){
+      return this.http.delete('http://localhost:3000/api/eventos/' + id)
+          .map(res => res.json());
+  }
+
+  updateEvento(evento){
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      return this.http.put('http://localhost:3000/api/eventos/'+evento._id, JSON.stringify(evento), { headers: headers })
+          .map(res => res.json());
+  }
+
+}
