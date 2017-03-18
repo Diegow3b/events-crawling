4cf400625a4c74b87203de3218cea5cf079f9319
Diego Vinicius <diegowebd@hotmail.com>
2017-03-18 10:09:24 -0300

    Removing searchPipe

--- client/src/app/eventos/eventos.component.ts
+++ client/src/app/eventos/eventos.component.ts
@@ -16,11 +16,11 @@ export class EventosComponent implements OnInit {
     eventos: Evento[];
     eventosQuant: number;
 
-    @ViewChild('input_category')
-    input_category: ElementRef;
+    // @ViewChild('input_category')
+    // input_category: ElementRef;
 
-    @ViewChild('input_city')
-    input_city: ElementRef;
+    // @ViewChild('input_city')
+    // input_city: ElementRef;
 
     _id: string;
     title: string;
@@ -33,15 +33,15 @@ export class EventosComponent implements OnInit {
     imagem: string;
     category: string;
 
+    dateFilter: string;
+    cityFilter: string;
+    categoryFilter: string;
+
     defaultImage: string;
 
     constructor(private eventosService: EventosService, private sanitizer: DomSanitizer) {
 
-        this.eventosService.getEventos()
-            .subscribe(eventos => {
-                this.eventos = eventos;
-                this.eventosQuant = eventos.length;
-            });
+        this.getAllEventos();
 
         this.defaultImage = 'https://d1gkntzr8mxq7s.cloudfront.net/58c9e8b1161a2-xs.jpg';
     }
@@ -54,6 +54,28 @@ export class EventosComponent implements OnInit {
         }
     }
 
+    filterEvents(){
+      event.preventDefault();
+      var filter = {
+          category: this.categoryFilter,
+          city: this.cityFilter,
+          date: this.dateFilter
+      }
+
+      this.eventosService.filterEvento(filter)
+            .subscribe(eventos => {
+                this.eventos = eventos;
+                if(eventos) this.eventosQuant = eventos.length;
+                console.log("Returning");
+                console.log(eventos);
+            });
+
+    }
+
+    setDateFilter(event){
+      this.dateFilter = event.target.attributes[1].value
+    }
+
     limparForm() {
         this.title = '';
         this.location = '';
@@ -62,6 +84,14 @@ export class EventosComponent implements OnInit {
         this.slug = '';
         this.description = '';
         this.category = '';
+    }
+
+    getAllEventos(){
+        this.eventosService.getEventos()
+            .subscribe(eventos => {
+                this.eventos = eventos;
+                this.eventosQuant = eventos.length;
+            });
     }
 
     addEvento(event) {
@@ -135,29 +165,29 @@ export class EventosComponent implements OnInit {
     }
 
     ngOnInit() {
-      let categoryObservable = Observable.fromEvent(this.input_category.nativeElement, 'keyup');
-      let cityObservable = Observable.fromEvent(this.input_city.nativeElement, 'keyup');
+      // let categoryObservable = Observable.fromEvent(this.input_category.nativeElement, 'keyup');
+      // let cityObservable = Observable.fromEvent(this.input_city.nativeElement, 'keyup');
 
-      categoryObservable.subscribe();
-      cityObservable.subscribe();
+      // categoryObservable.subscribe();
+      // cityObservable.subscribe();
     }
 
 }
 
-@Pipe({
-  name: 'searchPipe',
-  pure: false
-})
-export class SearchPipe implements PipeTransform {
-  transform(eventos: Evento[], searchKeys: any): any[] {
-      let category = searchKeys.category.toLowerCase();
-      let city = searchKeys.city.toLowerCase();
+// @Pipe({
+//   name: 'searchPipe',
+//   pure: false
+// })
+// export class SearchPipe implements PipeTransform {
+//   transform(eventos: Evento[], searchKeys: any): any[] {
+//       let category = searchKeys.category.toLowerCase();
+//       let city = searchKeys.city.toLowerCase();
 
-      if (!eventos) return
+//       if (!eventos) return
 
-      return eventos.filter(item => {
-          return item.category.toLowerCase() == category &&
-                 item.city.toLowerCase() == city;
-      });
-  }
-}
+//       return eventos.filter(item => {
+//           return item.category.toLowerCase() == category &&
+//                  item.city.toLowerCase() == city;
+//       });
+//   }
+// }
