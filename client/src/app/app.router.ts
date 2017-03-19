6fcf3e194023b971e1a382b151fe7b4f8b4465c2
Diego Vinicius <diegowebd@hotmail.com>
2017-03-19 14:46:38 -0300

    Adding Routers

---
+++ client/src/app/app.router.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { LoginComponent } from './login/login.component';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'events', pathMatch: 'full' },
+    { path: 'events', component: EventosComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'about', component: EventosComponent }, //TODO
+    { path: 'contact', component: EventosComponent } //TODO
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
