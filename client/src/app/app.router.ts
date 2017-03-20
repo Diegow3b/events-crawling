f0e2d27526fcd951451eb2261391463611c7e98d
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 12:41:04 -0300

    adding new admin route

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -6,13 +6,17 @@ import { EventosComponent } from './eventos/eventos.component';
 import { LoginComponent } from './login/login.component';
 import { EventosDetailComponent } from './eventos-detail/eventos-detail.component';
 
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+
 export const router: Routes = [
     { path: '', redirectTo: 'events', pathMatch: 'full' },
     { path: 'events', component: EventosComponent },
     { path: 'events-detail/:id', component: EventosDetailComponent },
     { path: 'login', component: LoginComponent },
     { path: 'about', component: EventosComponent }, //TODO
-    { path: 'contact', component: EventosComponent } //TODO
+    { path: 'contact', component: EventosComponent }, //TODO
+
+    { path: 'admin', component: DashboardComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
