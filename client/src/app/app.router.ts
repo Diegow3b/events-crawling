148dd077b295a9790cd0e6651f5ad73123294c3b
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 14:15:02 -0300

    Added new children routes to admin

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -6,7 +6,10 @@ import { EventosComponent } from './eventos/eventos.component';
 import { LoginComponent } from './login/login.component';
 import { EventosDetailComponent } from './eventos-detail/eventos-detail.component';
 
+import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { GraphsComponent } from './admin/graphs/graphs.component';
+import { EventoslistComponent } from './admin/eventos/eventoslist/eventoslist.component';
 
 export const router: Routes = [
     { path: '', redirectTo: 'events', pathMatch: 'full' },
@@ -16,7 +19,12 @@ export const router: Routes = [
     { path: 'about', component: EventosComponent }, //TODO
     { path: 'contact', component: EventosComponent }, //TODO
 
-    { path: 'admin', component: DashboardComponent }
+    { path: 'admin', component: AdminComponent, children: [
+        { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+        { path: 'dashboard', component: DashboardComponent },
+        { path: 'events/list', component: EventoslistComponent }
+    ]},
+
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
