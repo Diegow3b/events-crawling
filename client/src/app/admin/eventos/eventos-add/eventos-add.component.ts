6b2029835096e6b8a755042e4cf0487bc676fe87
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 20:23:19 -0300

    Fixing problemas at Eventos Add and List

--- client/src/app/admin/eventos/eventos-add/eventos-add.component.ts
+++ client/src/app/admin/eventos/eventos-add/eventos-add.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { EventosService } from '../../../services/eventos.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-eventos-add',
-  templateUrl: './eventos-add.component.html',
-  styleUrls: ['./eventos-add.component.css']
+    selector: 'app-eventos-add',
+    templateUrl: './eventos-add.component.html',
+    styleUrls: ['./eventos-add.component.css']
 })
 export class EventosAddComponent implements OnInit {
 
-  constructor() { }
+    title: string;
+    location: string;
+    city: string;
+    slug: string;
+    description: string;
+    imagem: string;
+    category: string;
+    start_date: any;
+    end_date: any;
+    producer: string;
 
-  ngOnInit() {
-  }
+    datepickerOpts: any;
+    timepickerOpts: any;
+
+    constructor(private eventosService: EventosService, private router: Router) {
+    }
+
+    ngOnInit() {
+    }
+
+    addEvento(event) {
+        event.preventDefault();
+        var newEvento = {
+            title: this.title,
+            location: this.location,
+            city: this.city,
+            start_date: new Date(this.start_date),
+            end_date: new Date(this.end_date),
+            slug: this.slug,
+            description: this.description,
+            imagem: this.imagem,
+            category: this.category,
+            producer: this.producer
+        }
+
+        this.eventosService.addEvento(newEvento)
+            .subscribe(evento => {
+                this.router.navigate(['events']);
+            });
+
+    }
 
 }
