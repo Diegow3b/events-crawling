1519380f30c44ec3d15d313633de9d0407868a76
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 14:17:39 -0300

    Created new component Admin as base to every template in admin

---
+++ client/src/app/admin/admin.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+    constructor() {
+        this.loadAdminCss();
+    }
+
+    ngOnInit() {
+    }
+
+    loadAdminCss() {
+        this.loadCSS('../src/static/admin/css/AdminLTE.min.css');
+        this.loadCSS('../src/static/admin/css/skins/_all-skins.min.css');
+    }
+
+    loadCSS(url) {
+        // Create link
+        let link = document.createElement('link');
+        link.href = url;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+
+        let head = document.getElementsByTagName('head')[0];
+        let links = head.getElementsByTagName('link');
+        let style = head.getElementsByTagName('style')[0];
+
+        // Check if the same style sheet has been loaded already.
+        let isLoaded = false;
+        for (var i = 0; i < links.length; i++) {
+            var node = links[i];
+            if (node.href.indexOf(link.href) > -1) {
+                isLoaded = true;
+            }
+        }
+        if (isLoaded) return;
+        head.insertBefore(link, style);
+    }
+
+}
