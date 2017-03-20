7806a0650ea3bb421081c1299df1b8cdc455681b
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 20:20:36 -0300

    Added EventosEditComponent and Ng2DateTimePickerMOdule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { EventosService } from './services/eventos.service';
 
 import { routes } from './app.router';
 
+import { Ng2DatetimePickerModule } from 'ng2-datetime-picker';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { LoginComponent } from './login/login.component';
@@ -17,6 +19,7 @@ import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { GraphsComponent } from './admin/graphs/graphs.component';
 import { EventosAddComponent } from './admin/eventos/eventos-add/eventos-add.component';
 import { EventosListComponent } from './admin/eventos/eventos-list/eventos-list.component';
+import { EventosEditComponent } from './admin/eventos/eventos-edit/eventos-edit.component';
 
 
 @NgModule({
@@ -29,14 +32,16 @@ import { EventosListComponent } from './admin/eventos/eventos-list/eventos-list.
     GraphsComponent,
     AdminComponent,
     EventosAddComponent,
-    EventosListComponent
+    EventosListComponent,
+    EventosEditComponent
     // SearchPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routes
+    routes,
+    Ng2DatetimePickerModule
   ],
   providers:[EventosService],
   bootstrap: [AppComponent]
