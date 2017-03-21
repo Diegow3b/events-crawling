6bce6978a0805ac21418c6ff76915f28641a4a28
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 22:00:18 -0300

    Adding producer attr in Evento

--- client/class/evento.ts
+++ client/class/evento.ts
@@ -1,12 +1,13 @@
 export class Evento{
-  _id : number;
+  _id : string;
   title: string;
   location: string;
   city: string;
-  start_date: any;
-  end_date: any;
+  start_date: Date;
+  end_date: Date;
   slug: string;
   description: string;
   category: string;
   producer: string;
+  imagem: string;
 }
